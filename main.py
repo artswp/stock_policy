9bcb25c79a9bec7a5e7a6322073fa8a0f8e9b082
artswp <artswp@qq.com>
2020-06-08 22:23:05 +0800

    改了好多

--- main.py
+++ main.py
@@ -31,24 +31,95 @@ nmc:流通市值
 2.今收>昨max
 
 """
-
+start_today='2020-06-08'
+end_last='2020-05-08'
 for index in today_all.index:
-    dropline=False
-    if today_all['open'][index] > today_all['settlement'][index] and today_all['trade'][index] > today_all['settlement'][index]:
-        dropline=False
-    else:
-        dropline=True
-    
-    if(dropline):
-        anti = today_all['high'][index] - today_all['trade'][index]
-        body = today_all['trade'][index] - today_all['open'][index]
-        if today_all['changepercent'][index] > 5.0 and today_all['changepercent'][index] < 9.0 and today_all['open'][index] - today_all['low'][index] == 0 and anti/body >1/3:
-            dropline=False
-        else:
-            dropline=True
-    if dropline:
-        today_all.drop([index],inplace=True)
-print(today_all)
+	#filter some not happen
+	#     if today_all['changepercent'][index] < 2.0 or today_all['settlement'][index] ==0 or today_all['changepercent'][index] > 8.0:
+	#         today_all.drop([index],inplace=True)
+	#         continue  
+
+	#temp data
+	settlement = today_all['settlement'][index] 
+	open       = today_all['open'][index]
+	trade      = today_all['trade'][index]
+	code       = today_all['code'][index]
+	name       = today_all['name'][index]
+	if code[0]=='3' or name[0]=='*':
+		continue #过滤掉创业板和带*的
+	
+	#下载最近20天的股价
+	hist=ts.get_hist_data(code,start=start_today,end=end_last)
+	
+	'''
+	date：日期
+	open：开盘价
+	high：最高价
+	close：收盘价
+	low：最低价
+	volume：成交量
+	price_change：价格变动
+	p_change：涨跌幅
+	ma5：5日均价
+	ma10：10日均价
+	ma20:20日均价
+	v_ma5:5日均量
+	v_ma10:10日均量
+	v_ma20:20日均量
+	turnover:换手率[注：指数无此项]
+	'''
+	#判断是否水上漂
+	#判断第一天涨幅是否大于8%则舍弃
+	if hist==None or hist.empty :
+		continue
+	if hist['p_change'][0] < 8.0:
+		 #循环判断前5个的收盘价是否都大于5日线，连续4日则列出来 
+		if hist['close'][0]>hist['ma5'][0] \
+			and hist['close'][1]>hist['ma5'][1] \
+			and hist['close'][2]>hist['ma5'][3] \
+			and hist['close'][3]>hist['ma5'][3]:
+			print(today_all['code'][index],",",today_all['name'][index],"连续4日5均线上")
+			continue
+
+
+
+
+	#jump space
+	open_percent= (open - settlement)/settlement *100
+	if open > settlement and trade > settlement and open_percent > 2.0:
+		print(today_all['code'][index],",",today_all['name'][index],"跳空选股")
+		continue
+
+
+	#goog line
+
+	anti = today_all['high'][index] - today_all['trade'][index]
+	body = today_all['trade'][index] - today_all['open'][index]
+	if body==0:
+		body=1
+	# print("today_all['changepercent'][index]:",today_all['changepercent'][index])
+	# print("today_all['changepercent'][low]:",today_all['low'][index])
+	if today_all['changepercent'][index] > 5.0 and today_all['changepercent'][index] < 9.0 and today_all['open'][index] - today_all['low'][index] == 0 and anti/body >1/3:
+		print(today_all['code'][index],",",today_all['name'][index],"捉腰选股")
+		continue
+	# if dropline:
+	#     today_all.drop([index],inplace=True)
+# print(today_all)
+
+
+
+
+'''
+跳空：
+最小跳空2%，收涨8%内
+1.周线跳空:
+1.1周线跳空，比如周一产生跳空
+1.2周线跳空5周线，前1~2天温和放量
+2.日线跳空，周线连续
+2.1日线跳空，不产生周线跳空，
+2.1跳空日线5日线,前1~2天温和放量极好，突然放量需要观察1~2天
+2.1跳空日线20日线,相当于跳空周线
+'''
 
 '''
 捉腰
@@ -56,4 +127,29 @@ print(today_all)
 涨幅>6%
 上影线小于涨幅1/3
 
+前1~2天温和放量，当天倍量
+
 '''
+
+'''
+4~5连阳
+连续4日股价范围在5日线上
+'''
+
+'''
+吞噬
+
+'''
+
+'''
+特点：
+攻击线：
+1.1分为周攻击、日攻击
+1.2攻击前温和放量
+1.3攻击时放量
+1.4攻击后稳住攻击线
+1.5攻击方式为跳空、大阳线、吞噬
+活力线
+1.1
+'''
+
